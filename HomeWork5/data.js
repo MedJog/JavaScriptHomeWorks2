99f4e90bf311875f6747d6c293dd9984d7838830
Svetlana <medjog@gmail.com>
2024-06-12 16:07:03 +0400

    HomeWork5

---
+++ HomeWork5/data.js
@@ -0,0 +1,42 @@
+const data = `[
+  {
+    "postId": 1,
+    "id": 1,
+    "avatar": "img/1.jpg",
+    "name": "Devil",
+    "email": "Eliseo@gardner.biz",
+    "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos ntempora quo necessitatibus ndolor quam autem quasi nreiciendis et nam sapiente accusantium"
+  },
+  {
+    "postId": 1,
+    "id": 2,
+    "avatar": "img/2.jpg",
+    "name": "Knight",
+    "email": "Jayne_Kuhic@sydney.com",
+    "body": "est natus enim nihil est dolore omnis voluptatem numquam net omnis occaecati quod ullam at nvoluptatem error expedita pariatur nnihil sint nostrum voluptatem reiciendis et"
+  },
+  {
+    "postId": 1,
+    "id": 3,
+    "avatar": "img/3.jpg",
+    "name": "Hulk",
+    "email": "Nikita@garfield.biz",
+    "body": "quia molestiae reprehenderit quasi aspernatur naut expedita occaecati aliquam eveniet laudantium nomnis quibusdam delectus saepe quia accusamus maiores nam est ncum et ducimus et vero voluptates excepturi deleniti ratione"
+  },
+  {
+    "postId": 1,
+    "id": 4,
+    "avatar": "img/4.jpg",
+    "name": "Robot",
+    "email": "Lew@alysha.tv",
+    "body": "non et atque noccaecati deserunt quas accusantium unde odit nobis qui voluptatem nquia voluptas consequuntur itaque dolor net qui rerum deleniti ut occaecati"
+  },
+  {
+    "postId": 1,
+    "id": 5,
+    "avatar": "img/5.jpg",
+    "name": "Tiger",
+    "email": "Hayden@althea.biz",
+    "body": "harum non quasi et ratione ntempore iure ex voluptates in ratione nharum architecto fugit inventore cupiditate nvoluptates magni quo et"
+  }
+]`;
